b52ebeb8fb965e82071072fd11811b345ea11eb7
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2018-12-22 12:38:40 +0000

    Affinage du design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { CdService } from './../services/CdService';
-import { BookService } from './../services/BookService';
-import { SettingsPage } from './../pages/settings/settings';
-import { LendCdPage } from './../pages/cds/lendcd/lendcd';
-import { CdListPage } from './../pages/cds/cdList/cdlist';
-import { LendBookPage } from './../pages/books/lendbook/lendbook';
-import { TabsPage } from './../pages/tabs/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { CdService } from './../services/CdService';
+import { BookService } from './../services/BookService';
+
 import { MyApp } from './app.component';
+import { SettingsPage } from './../pages/settings/settings';
+import { LendCdPage } from './../pages/cds/lendcd/lendcd';
+import { CdListPage } from './../pages/cds/cdList/cdlist';
+import { LendBookPage } from './../pages/books/lendbook/lendbook';
+import { TabsPage } from './../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
 import { BookListPage } from '../pages/books/booklist/booklist';
 
@@ -38,7 +39,7 @@ import { BookListPage } from '../pages/books/booklist/booklist';
     BookListPage,
     LendBookPage,
     CdListPage,
-    LendBookPage,
+    LendCdPage,
     SettingsPage
   ],
   providers: [
